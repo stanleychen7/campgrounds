c32e61daeffce6afe4068e3272987db9ff23e37b
stanleychen7 <stanleychen934@gmail.com>
2025-07-29 15:39:24 -0400

    CRUD

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override')
+
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -19,16 +23,43 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async(req, res) => {
-    const camp = new Campground({
-        title: 'Backyard',
-        description: 'Free'
-    })
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
+})
 
-    await camp.save()
-    res.send(camp);
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground})
+})
+
+app.get('/campgrounds/:id/edit', async(req, res) =>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground})
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
 })
 
 app.listen(3000, () => {
-    console.log('Port 3000')
-})
+    console.log('Serving on localhost:3000');
+});
